430d350db4a11b795673115cb73aa96dd7f0c9d6
Ciara Massyn <156077962+CiaraMassyn@users.noreply.github.com>
2024-01-11 23:32:59 +0200

    Adding Adapter Files

---
+++ Adapters/ImageAdapter.cs
@@ -0,0 +1,12 @@
+﻿using Services.Interfaces;
+
+namespace Adapters
+{
+    public class ImageAdapter : IImageAdapter
+    {
+        public string GenerateDog()
+        {
+            return "https://images.dog.ceo/breeds/hound-ibizan/n02091244_596.jpg";
+        }
+    }
+}
